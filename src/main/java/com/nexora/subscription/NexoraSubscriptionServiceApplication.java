b3cdeb8a205a060ef5934533bf65f25f0cc21990
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-03 16:39:50 +0300

    chore: initialize subscription service project

---
+++ src/main/java/com/nexora/subscription/NexoraSubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraSubscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraSubscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexora/subscription/NexoraSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexora.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraSubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
